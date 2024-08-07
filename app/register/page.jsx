d0b9b6c1484ba52f5c7774c12aa22ff64ee22aa2
lefline <issamkharbache2@gmail.com>
2024-08-07 13:58:46 +0100

    adding loading spinner component

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,28 +1,37 @@
 "use client";
 import Button from "@/components/rootComponents/Button";
+import LoadingSpinner from "@/components/rootComponents/LoadingSpinner";
 import { validateEmail } from "@/utils/validateEmail";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const RegisterPage = () => {
+  //states
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+
+  //
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
+    //getting data
     const fullName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-
+    //validation email
     if (!validateEmail(email)) {
       setError("Please enter a valid email");
       return;
     }
+    //validating password
     if (!password || password.length < 8) {
       setError("Password is required and must be at least 8 characters");
       return;
     }
     try {
+      //making api call
       const res = await fetch("/api/register", {
         method: "POST",
         headers: {
@@ -36,13 +45,16 @@ const RegisterPage = () => {
       });
       if (res.status === 400) {
         setError("User with this email already registered");
+        setLoading(false);
       }
       if (res.status === 200) {
         setError("");
+        setLoading(false);
         router.push("/login");
       }
     } catch (error) {
       setError("Error, please try again");
+      setLoading(false);
       console.log(error);
     }
   };
@@ -82,13 +94,20 @@ const RegisterPage = () => {
               required
               type="password"
               placeholder="Password"
-              className={`h-16 border-2 border-gray-300 focus:border-yellow-400 outline-none px-4 rounded-md `}
+              className={`h-16 border-2 border-gray-300 focus:border-yellow-400  outline-none px-4 rounded-md `}
             />
-            <Button
-              label="Sign up"
-              cn="bg-yellow-400 hover:bg-yellow-500"
-              type="submit"
-            />
+            {loading ? (
+              <Button
+                label={<LoadingSpinner />}
+                cn="bg-yellow-200 opacity-50 pointer-events-none flex items-center justify-center"
+              />
+            ) : (
+              <Button
+                label="Sign up"
+                cn="bg-yellow-400 hover:bg-yellow-500"
+                type="submit"
+              />
+            )}
           </form>
           <div className="text-center text-gray-500 mt-4">- OR -</div>
           <h1 className="mt-4 text-sm gap-2 flex justify-center">
